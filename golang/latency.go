d323883c85fc0a710bb6ce07fd884688f993eb52
Samuel Ortiz <sameo@linux.intel.com>
2017-06-30 13:52:41 +0200

    latency: Save latencies array
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- golang/latency.go
+++ golang/latency.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"flag"
+	"os"
 	"runtime/debug"
 	"sync"
 	"time"
@@ -24,6 +25,25 @@ var worstLatency time.Duration
 var bestLatency time.Duration
 var averageLatency time.Duration
 
+func storeLatencies(path string, latencies []time.Duration) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	for _, l := range latencies {
+		latency := []byte(fmt.Sprintf("%f\n", (float32)(l)/1000))
+		_, err = f.Write(latency)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 var bufPool = sync.Pool{
 	New: func() interface{} {
 		return make(message, bufferSize)
@@ -38,8 +58,9 @@ func newMessage(pool bool) message {
 	return make(message, bufferSize)
 }
 
-func idleThread(b *buffer, cycles, period, buffers int, usePool bool, latencies *[]time.Duration, wg *sync.WaitGroup) {
+func idleThread(b *buffer, cycles, period, buffers int, usePool bool, latenciesFile string, wg *sync.WaitGroup) {
 	var templ pb.ProgressBarTemplate = `{{string . "Cycles"}}{{counters . }} {{bar . }} {{percent . }}{{string . ""}}`
+	var latenciesArray []time.Duration
 	sleepPeriod := (time.Duration)(period) * time.Millisecond
 	bestLatency = time.Minute
 
@@ -80,7 +101,7 @@ func idleThread(b *buffer, cycles, period, buffers int, usePool bool, latencies 
 			bestLatency = latency
 		}
 
-		*latencies = append(*latencies, latency)
+		latenciesArray = append(latenciesArray, latency)
 
 		if usePool {
 			for j := 0; j < buffers; j++ {
@@ -91,19 +112,23 @@ func idleThread(b *buffer, cycles, period, buffers int, usePool bool, latencies 
 	}
 
 	bar.Finish()
+
+	if latenciesFile != "" {
+		storeLatencies(latenciesFile, latenciesArray)
+	}
 }
 
 func main() {
 	var wg sync.WaitGroup
 	var b buffer
 	var gcStats debug.GCStats
-	var latencies []time.Duration
 
 	cycles := flag.Int("cycles", 500, "number of sleeping cycles")
 	sleepPeriod := flag.Int("period", 100, "Sleeping period (in milliseconds)")
 	numBuffer := flag.Int("buffers", 10, "Number of allocated buffer per cycle")
 	usePool := flag.Bool("no-pool", false, "Do not use golang pools for memory allocations")
 	debugPause := flag.Bool("debug", false, "Dump all GC pauses and latencies")
+	latenciesFile := flag.String("file", "", "File to store all latencies in microseconds")
 	flag.Parse()
 
 	fmt.Printf("%d cycles - %dms sleep period - %d buffers allocated per cycle - Golang pools %v\n",
@@ -111,15 +136,11 @@ func main() {
 
 	wg.Add(1)
 
-	go idleThread(&b, *cycles, *sleepPeriod, *numBuffer, !(*usePool), &latencies, &wg)
+	go idleThread(&b, *cycles, *sleepPeriod, *numBuffer, !(*usePool), *latenciesFile, &wg)
 
 	wg.Wait()
 
 	fmt.Printf("Latency: [Avg %vµs, Best %v, Worst %v]\n", (averageLatency.Nanoseconds()/(int64)(*cycles))/1000, bestLatency, worstLatency)
-
-	if *debugPause {
-		fmt.Printf("Latencies: %v\n", latencies)
-	}
 
 	debug.ReadGCStats(&gcStats)
 
